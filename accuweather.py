1822ccf025c8fefa941e3bc54a37e1de4b920abe
armin <armin.aligholian@gmail.com>
2020-07-07 11:44:36 -0700

    fixed the connection of API

--- accuweather.py
+++ accuweather.py
@@ -11,11 +11,11 @@ country_code='US'
 city='Riverside'
 def get_location(country_code,city):
     
-    search_address="http://www.accuweather.com/en/us/riverside-ca/92506/hourly-weather-forecast/327146?day=1&hbhhour=20&lang=en-us"
+    search_address="http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&q=Riverside"
     print(search_address)
     with urllib.request.urlopen(search_address) as search_address:
         print(search_address)
-    #     data=json.loads(search_address.read().decode())
-    # print(data)
+        data=json.loads(search_address.read().decode())
+    print(data)
     
 get_location(country_code, city)
