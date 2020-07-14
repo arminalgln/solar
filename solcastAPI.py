5ff0d1668f6b3c8ccc241d5a85a7cfdc75ac93c2
Armin Aligholian <aalig002@ucr.edu>
2020-07-13 18:03:46 -0700

    opencage api for lat and lng based on address and get the forecast from solcast api

--- solcastAPI.py
+++ solcastAPI.py
@@ -2,47 +2,71 @@ import json
 import time
 import urllib.request
 import solcast
-#%%
-API_key='Cy_svKl2MNHobcsBWczeH-6MQ6cp2N6N'
-base_url='https://api.solcast.com.au/'
-resource_id='a21d-7ef6-dc3f-19b3'###this is for Irvine and ETAP office
-weather_forecast_url='/weather_sites/'+resource_id+'/forecasts'
-#%%
-radiation_forecasts = solcast.get_radiation_forecasts(-35, 149, API_key)
-#%%
-f=radiation_forecasts.forecasts
+
+from opencage.geocoder import OpenCageGeocode
 
 #%%
-country_code='US'
-city=input('City Name')
-# city='Riverside'
-def get_location(country_code,city):
+# =============================================================================
+# take address and return lat and lng
+# =============================================================================
+address=input('What is your address?  ')
+location_api_key='23e6edd3ccc7437b90c589fd7c9c6213'
+geocoder = OpenCageGeocode(location_api_key)
+
+whole_location_info = geocoder.geocode(address)[0]
+geo=whole_location_info['geometry']
+lat,lng=geo['lat'],geo['lng']
+
+
+#%%
+# =============================================================================
+# get lat and lng and return forecast irredication data
+# =============================================================================
+solcast_API_key='osmO54Z_7TKYMgJFi3vrQenczYLbErBk'
+
+radiation_forecasts = solcast.get_radiation_forecasts(lat, lng, solcast_API_key)
+
+radiation_forecasts_data=radiation_forecasts.forecasts
+
+
+
+
+
+
+#%%
+
+
+# #%%
+# country_code='US'
+# city=input('City Name')
+# # city='Riverside'
+# def get_location(country_code,city):
     
-    search_address="http://dataservice.accuweather.com/locations/v1/cities/"+country_code+"/search?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&q="+city+""
-    # print(search_address)
-    with urllib.request.urlopen(search_address) as search_address:
-        # print(search_address)
-        data=json.loads(search_address.read().decode())
-    # print(data)
-    return data[0]['Key']
-    # print(location_key)
+#     search_address="http://dataservice.accuweather.com/locations/v1/cities/"+country_code+"/search?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&q="+city+""
+#     # print(search_address)
+#     with urllib.request.urlopen(search_address) as search_address:
+#         # print(search_address)
+#         data=json.loads(search_address.read().decode())
+#     # print(data)
+#     return data[0]['Key']
+#     # print(location_key)
     
     
-# def get_forecast(location_key):
-#     12_daily_forecast="http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"+location_key+"?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&details=true"
-#     with urllib.request.urlopen(12_daily_forecast) as 12_daily_forecast:
-#         data=json.loads(12_daily_forecast.read().decode())
+# # def get_forecast(location_key):
+# #     12_daily_forecast="http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"+location_key+"?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&details=true"
+# #     with urllib.request.urlopen(12_daily_forecast) as 12_daily_forecast:
+# #         data=json.loads(12_daily_forecast.read().decode())
+# #     return data
+
+# def get_historical(location_key):
+#     historical="http://dataservice.accuweather.com/currentconditions/v1/"+location_key+"/historical/24?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&details=true"
+#     with urllib.request.urlopen(historical) as historical:
+#         data=json.loads(historical.read().decode())
 #     return data
-
-def get_historical(location_key):
-    historical="http://dataservice.accuweather.com/currentconditions/v1/"+location_key+"/historical/24?apikey=f7p6tZSmiDE0F4xF0nLMH44qGJYSJhY7&details=true"
-    with urllib.request.urlopen(historical) as historical:
-        data=json.loads(historical.read().decode())
-    return data
-location_key=get_location(country_code, city)
-# data=get_forecast(location_key)
-historical=get_historical(location_key)
-#%%
-for i in historical:
-    print(i['CloudCover'])
+# location_key=get_location(country_code, city)
+# # data=get_forecast(location_key)
+# historical=get_historical(location_key)
+# #%%
+# for i in historical:
+#     print(i['CloudCover'])
 
