b23b0c1820f997c4ff1f99ffdb8a4ecc68007e5f
armin <armin.aligholian@gmail.com>
2020-09-25 12:00:56 -0700

    last day

--- solcastAPI.py
+++ solcastAPI.py
@@ -70,3 +70,92 @@ pred.to_pickle(f_dst)
 act.to_pickle(a_dst)
 print('the files for %s has been saved!' % day)
 #%%
+#7 days data for different sites with 6 kWh capacity
+
+lats = [17.63506, 17.73534, 10.19835, 22.86274, 19.72728]
+lngs = [94.58346, 96.22166, 98.60683, 93.62167, 97.07478]
+caps = [6, 6, 6, 5.2, 6]
+
+
+
+
+
+
+for i in range(len(lats)):
+    lat = lats[i]
+    lng = lngs[i]
+    capacity = caps[i]
+    hours = 168
+    solcast_API_key = 'Cy_svKl2MNHobcsBWczeH-6MQ6cp2N6N'
+    base_url_estimated = 'https://api.solcast.com.au//world_pv_power/estimated_actuals?' \
+                         'latitude=%s&longitude=%s&capacity=%s&format=json&api_key=%s&hours=%s' % (
+                         lat, lng, capacity, solcast_API_key, hours)
+
+    weburl = urllib.request.urlopen(base_url_estimated)
+
+    data = weburl.read()
+    data = json.loads(data)
+    data = pd.DataFrame(data['estimated_actuals'])
+
+    data.to_csv('data/new_site/' + str(lat) + '-' + str(lng) + '-' + str(capacity) + 'past' +'.csv')
+
+    plt.plot(data['pv_estimate'])
+    plt.show()
+
+
+    base_url_estimated = 'https://api.solcast.com.au//world_pv_power/forecasts?' \
+                         'latitude=%s&longitude=%s&capacity=%s&format=json&api_key=%s&hours=%s' % (
+                         lat, lng, capacity, solcast_API_key, hours)
+
+    weburl = urllib.request.urlopen(base_url_estimated)
+
+    data = weburl.read()
+    data = json.loads(data)
+    data = pd.DataFrame(data['forecasts'])
+
+    data.to_csv('data/new_site/' + str(lat) + '-' + str(lng) + '-' + str(capacity) + 'forecasts' +'.csv')
+
+    # plt.plot(data['pv_estimate'])
+    # plt.show()
+
+#%%
+url = 'https://api.weather.com/v1/geocode/33.4/-117/forecast/hourly/24hour.json?units=m&language=en-US&apiKey=5424e9662cbf4bc3a4e9662cbf4bc3fe'
+
+api_key = '5424e9662cbf4bc3a4e9662cbf4bc3fe'
+url = 'https://api.weather.com/v1/geocode/33.4/117/forecast/hourly/6hour.json?units=m&language=en-US&apiKey=%s' %(api_key)
+
+weburl = urllib.request.urlopen(url)
+data = weburl.read()
+data = json.loads(data)
+#%%
+data = pd.DataFrame(data['forecasts'])
+data.to_csv('data/weatherCOM6dayshourly.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
